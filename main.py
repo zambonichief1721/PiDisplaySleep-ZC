83837a7796d6df898f3b033f83fd6ed4ce9d9105
ZamboniChief1721 <gregorio.stephen@gmail.com>
2024-01-16 16:05:12 -0600

    Added second ip option

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from os import popen,system
 from time import sleep
 state=1 #State of the display 1 On 0 Off
-ip="192.168.1.3" #Enter the IP address of the device that should keep the display awake
+ip_1="192.168.1.3" #Enter the IP address of the device that should keep the display awake
+ip_2=""
 
 while True:
-    nmap_out=str(popen('nmap -sP '+ip).read()) #nmap command to scan on the given IP address
+    nmap_out=str(popen('nmap -sP '+ip_1).read()) #nmap command to scan on the given IP address
+    nmap_out_2=str(popen('nmap -sP '+ip_2).read())
     sleep(2)
     
-    if nmap_out.find('latency') == -1:  #looks for the word "latency" in the output
+    if (nmap_out.find('latency') == -1) and (nmap_out_2.find('latency') == -1):  #looks for the word "latency" in the output
         if state==0 :                   #this nested if makes sure that commands are not repeated
             pass
         else :
             system('vcgencmd display_power 0')  #Bash command that turns off the display
             state=0                             #Updating the display state variable
 
-    elif nmap_out.find('latency') > 1:
+    elif (nmap_out.find('latency') > 1) and (nmap_out_2.find('latency') > 1):
         if state==1:
             pass
         else :
