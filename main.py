0ab3aec51da009f89bfe28d6dd27734a93767587
ZamboniChief1721 <gregorio.stephen@gmail.com>
2024-04-26 15:16:39 -0500

    updating code to do checks a different way

--- main.py
+++ main.py
@@ -2,36 +2,67 @@ from os import popen,system
 from time import sleep
 import datetime
 
-state=1 #State of the display 1 On 0 Off
-ip_1="192.168.80.207" #Enter the IP address of the device that should keep the display awake
-ip_2="192.168.40.51"
+home_state = 1
+time_state = 1
 
+def set_vars():
+    global home_state,time_state
+    home_state = 1 #State of the display 1 On 0 Off
+    time_state = 1 #State of the display 1 On 0 Off
 
-while True:
+def check_phone_home():
+    global home_state
+    ip_1="192.168.80.207" #Enter the IP address of the device that should keep the display awake
+    ip_2="192.168.40.51"
+    nmap_out=str(popen('nmap -sP '+ip_1).read()) #nmap command to scan on the given IP address
+    nmap_out_2=str(popen('nmap -sP '+ip_2).read())
+    sleep(4)
+    
+    if nmap_out.find('latency') == -1 and nmap_out_2.find('latency') == -1:  #looks for the word "latency" in the output
+        if home_state==0 :                   #this nested if makes sure that commands are not repeated
+            pass
+        else :
+            #system('xset -display :0 dpms force off')  #Bash command that turns off the display
+            home_state=0                             #Updating the display state variable
+
+    elif nmap_out.find('latency') > 1 or nmap_out_2.find('latency') > 1:
+        if home_state==1:
+            pass
+        else :
+            #system('xset -display :0 dpms force on') #Bash command to turn on the display
+            home_state=1
+    return home_state
+
+def check_time():
+    global time_state
     current_time = datetime.datetime.now()
     hour = current_time.hour
-
-    while hour <= 22 or hour >= 6:
-
-        nmap_out=str(popen('nmap -sP '+ip_1).read()) #nmap command to scan on the given IP address
-        nmap_out_2=str(popen('nmap -sP '+ip_2).read())
-        sleep(4)
     
-        if nmap_out.find('latency') == -1 and nmap_out_2.find('latency') == -1:  #looks for the word "latency" in the output
-            if state==0 :                   #this nested if makes sure that commands are not repeated
-                pass
-            else :
-                system('xset -display :0 dpms force off')  #Bash command that turns off the display
-                state=0                             #Updating the display state variable
+    if hour>=6 and hour<=22:  #looks for the word "latency" in the output
+        if time_state==1:                   #this nested if makes sure that commands are not repeated
+            pass
+        else :
+            #system('xset -display :0 dpms force off')  #Bash command that turns off the display
+            time_state=1                             #Updating the display state variable
 
-        elif nmap_out.find('latency') > 1 or nmap_out_2.find('latency') > 1:
-            if state==1:
-                pass
-            else :
-                system('xset -display :0 dpms force on') #Bash command to turn on the display
-                state=1
+    elif hour<=6 or hour>=22:
+        if time_state==0:
+            pass
+        else :
+            #system('xset -display :0 dpms force on') #Bash command to turn on the display
+            time_state=0
+    return time_state
 
-    system('xset -display :0 dpms force off')
-    state=0
-    
-    sleep(10) #Scan rate in seconds
+def main():
+    set_vars()
+    while True:
+        time_state = check_time()
+        home_state = check_phone_home()
+        if home_state==0 or time_state==0:
+            print('xset -display :0 dpms force off')
+        elif home_state==1 and time_state==1:
+            print('xset -display :0 dpms force on')
+        sleep(10) #Scan rate in seconds
+
+if __name__ == "__main__":
+    main()
